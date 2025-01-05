c0ffdf7caa499993f23bff79f95e14542c0cbb1f
Gadg8eer <64449092+Gadg8eer@users.noreply.github.com>
2025-01-04 18:36:57 -0700

    Changed GRF name
    If reldred wants credit, I'm willing to add him if he's willing to re-open communications for the purpose of collaboration (and yes, not drama). Otherwise he can forget it. No drama means no drama, blocking me pre-emptively is drama as much as anything I've been told to stop doing.

--- opengfx-mars-houses/pybuild.bat
+++ opengfx-mars-houses/pybuild.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 :start
-python nml_patcher.py -f "ogfx-mars-houses-remod.pnml" -o "ogfx-mars-houses-remod.nml" -b 1 -v 1
-nmlc ogfx-mars-houses-remod.nml -o ogfx-mars-houses-remod.grf
+python nml_patcher.py -f "ogfx-mars-houses-rehabs.pnml" -o "ogfx-mars-houses-rehabs.nml" -b 1 -v 1
+nmlc ogfx-mars-houses-rehabs.nml -o ogfx-mars-houses-rehabs.grf
 PAUSE
 goto start
